61387388cc1b4c2a9603ad961a5ad836c3ef0d45
Ben Cornelius <ben.z.cornelius@gmail.com>
2015-06-28 23:20:26 -0500

    Found some time to commit code

--- problem3.py
+++ problem3.py
@@ -1,15 +1,25 @@
-# 600851475143
-def factor(num):
-    list = []
-    if not num % 2:
-        half = ((num - 1)/2)
-    else:
-        half = (num/2)
-    i=1
-    while i < half:
-        if num % i == 0:
-            list.append(i)
-        i += 1
-    return list
+import math
 
-print factor(600851475143)
+def get_factors(num):
+    factors = []
+    i = 2
+    while i < math.sqrt(num):
+        if num % i == 0:
+            factors.append(i)
+        i += 1
+    return factors
+
+def is_list_prime(list):
+    prime_numbers = []
+    for item in list:
+        i = 2
+        flag = True
+        while i < math.sqrt(item):
+            if item % i == 0:
+                flag = False
+            i += 1
+        if flag == True:
+            prime_numbers.append(item)
+    return prime_numbers
+
+print max(is_list_prime(get_factors(600851475143)))
